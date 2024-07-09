12df94c7491f8edfe2e98569d3a8c09cb6d3ee41
Fried-wogns20 <84609921+wogns-kim@users.noreply.github.com>
2024-07-09 15:51:07 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 김재훈 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
